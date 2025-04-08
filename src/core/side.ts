41a1ecc6260993c90b353fea42eeb2bf87131b38
Jesus Chacon <jesuszen2.6@gmail.com>
2025-04-07 23:21:18 -0400

    add: logic for opengraph global, and core side

---
+++ src/core/side.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+const links = {
+  instagram: "https://instagram.com/pata-mia",
+  githubAccount: "https://github.com/Afrozens",
+};
+
+export const siteConfig = {
+  name: "Pata Mia",
+  description:
+    "Pata Mia es la plataforma líder que conecta veterinarios y cuidadores profesionales con dueños de mascotas. Publica tus servicios como profesional, gestiona citas y recibe pagos seguros. Para clientes: encuentra cuidadores certificados, agenda servicios y paga con total confianza. ¡Cuidamos de tus mascotas como si fueran nuestras!",
+  url: "https://pata-mia.app",
+  links,
+};
