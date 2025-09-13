14dbb3879b57b8e65555f0c77921e0f4f4570db1
Jesus Chacon <jesuszen2.6@gmail.com>
2025-09-13 12:44:56 -0400

    update auth layout translation

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -6,12 +6,14 @@ import Link from 'next/link';
 
 import Footer from '@/components/commons/Footer';
 import ButtonRegister from '@/components/commons/buttons/ButtonRegister';
+import { useTranslations } from 'next-intl';
 
 interface Props extends PropsWithChildren {
   withAd?: boolean;
 }
 
 const AuthLayout = ({ children, withAd = true }: Props) => {
+  const t = useTranslations('input')
   return (
     <>
       <section className="w-full min-h-screen flex">
@@ -25,7 +27,7 @@ const AuthLayout = ({ children, withAd = true }: Props) => {
             <header className="absolute top-0 z-[999] bg-transparent flex w-full p-10 2xl:px-20 items-center justify-between h-[51px]">
               <Link href={'/'}>
                 <div className="cursor-pointer -mt-2 mb-1 flex w-fit items-center gap-2 font-medium transition-all hover:-translate-x-2 hover:opacity-80">
-                  <LeftOutlined className="text-[14px]" /> Volver
+                  <LeftOutlined className="text-[14px]" /> {t('back')}
                 </div>
               </Link>
               <ButtonRegister defaultButton />
